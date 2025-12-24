d96e3629f37739e0b344faf56dd814e3b3859e1b
PeterAlbus@ECUST_A6000 <wuhongdb@163.com>
2025-12-24 15:30:11 +0800

    增加修改后的 apart 框架代码。

--- utils/factory.py
+++ utils/factory.py
@@ -2,6 +2,8 @@ def get_model(model_name, args):
     name = model_name.lower()
     if name == "apart":
         from models.apart import Learner
+    elif name == "apart_v2":
+        from models.apart_v2 import Learner
     else:
         assert 0
     
